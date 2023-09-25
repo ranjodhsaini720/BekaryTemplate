bc8fe250f45899d295ac28f09b248fc01d41378b
ranjodhsaini720 <ranjodhsaini720@gmail.com>
2023-09-25 11:29:26 +0530

    Bekary Website Template Added.

---
+++ bakery.js
@@ -0,0 +1,69 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1000,
+    },
+    breakpoints: {
+        100: {
+            slidesPerView: 1,
+            spaceBetween: 24,
+            resistanceRatio: 0.85
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+            resistanceRatio: 0.85
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 28,
+            resistanceRatio: 0.85
+        },
+        980: {
+            slidesPerView: 4,
+            spaceBetween: 19,
+            resistanceRatio: 0.85
+        },
+        1280: {
+            slidesPerView: 5,
+            spaceBetween: 32,
+            resistanceRatio: 0
+        }
+    }
+});
+
+//  
+//  
+
+var swiper = new Swiper(".mySwiper1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+
+//   
+// 
+
+// window.onscroll = function () { scroll() };
+// function scroll () {
+//     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+//         document.getElementById("nav").style.backgroundColor="#00091dac";
+//         document.getElementById("nav").style.color="white";
+//     }
+//     else{
+//         document.getElementById('nav').style.backgroundColor="white"
+//     }
+// }
